1fe4d3fee020fbdb91243ab199c33704366f38e9
Raeumlichkeit <138493564+Raeumlichkeit@users.noreply.github.com>
2023-08-09 16:38:47 +0200

    Update rente_timer.ino

--- rente_timer.ino
+++ rente_timer.ino
@@ -1,13 +1,8 @@
-
-#include <DS1307RTC.h>
-#include <TimeLib.h>
 #include <Adafruit_GFX.h>
 #include <RGBmatrixPanel.h>
+#include <RTClib.h>
 
-int Jahre;
-int Monate;
-int Tage = "Rest von diesem";
-
+RTC_DS1307 rtc;
 
 #define CLK 8
 #define LAT A3
@@ -18,29 +13,71 @@ int Tage = "Rest von diesem";
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
 
-  void setup() {
+void setup() {
   Serial.begin(9600);
   matrix.begin();
-  setSyncProvider(RTC.get);
+  rtc.begin();
   matrix.setTextColor(matrix.Color333(255, 255, 255));
 }
 
 
 void loop() {
-  Berechnung();
+
+  DateTime futureDate(2025, 07, 01, 0, 0, 0);
+  DateTime now = rtc.now();
+  int remainingYears = futureDate.year() - now.year();
+  int remainingMonths = futureDate.month() - now.month();
+  int remainingDays = futureDate.day() - now.day();
+  int remainingHours = futureDate.hour() - now.hour();
+  int remainingMinutes = futureDate.minute() - now.minute();
+  int remainingSeconds = futureDate.second() - now.second();
+
+  if (remainingSeconds < 0) {
+    remainingMinutes--;
+    remainingSeconds += 60;
+  }
+  if (remainingMinutes < 0) {
+    remainingHours--;
+    remainingMinutes += 60;
+  }
+  if (remainingHours < 0) {
+    remainingDays--;
+    remainingHours += 24;
+  }
+  if (remainingDays < 0) {
+    remainingMonths--;
+    remainingDays += daysInMonth(now.year(), now.month());
+  }
+  if (remainingMonths < 0) {
+    remainingYears--;
+    remainingMonths += 12;
+  }
+
   matrix.setCursor(1, 1);
   matrix.print("NUR  NOCH");
   delay(1000);
   matrix.fillScreen(0);
   matrix.setCursor(1, 1);
-  matrix.println(Jahre);
+  matrix.println(remainingYears);
   matrix.print("Jahre");
   delay(1000);
   matrix.fillScreen(0);
   matrix.setCursor(1, 1);
-  matrix.println(month());
+  matrix.println(remainingMonths);
   matrix.print("Monate");
   delay(1000);
   matrix.fillScreen(0);
   matrix.swapBuffers(true);
+}
+
+
+int daysInMonth(int year, int month) {
+  int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int result = days[month - 1];
+
+  if (month == 2 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) {
+    result++; // Schaltjahr: Februar hat 29 Tage
+  }
+
+  return result;
 }
